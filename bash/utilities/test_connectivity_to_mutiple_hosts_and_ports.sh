5c822c7fe3eb6cc5d2edeb6abec8e0ba7a833b23
Marlon Thomas <marlon-thomas@users.noreply.github.com>
2017-04-07 20:31:06 +0100

    Update test_connectivity_to_mutiple_hosts_and_ports.sh

--- bash/utilities/test_connectivity_to_mutiple_hosts_and_ports.sh
+++ bash/utilities/test_connectivity_to_mutiple_hosts_and_ports.sh
@@ -1,27 +1,26 @@
-#!/bin/bash
+##!/bin/bash
 
 main()
 {
  clear
- servers_to_test=("hostname1" "hostname2")
- uklpdpeci120a_ports=("1621")
- uklpdpeci121a_ports=("1622")
+ targets_to_test=("server1:1622:1621" "server2:1622")
+
  hline="====================================================================================="
  my_ip_address=$(ifconfig | grep -C 1 eth0 | tail -n 1 | awk '{print $2}' | sed 's/[A-Za-z:]//g')
  echo $hline
  echo "Testing connectivity from $HOSTNAME ( $my_ip_address ) ..."
  echo $hline
- i=0
- for s in "${servers_to_test[@]}" ; do
-   target_full_hostname=$(get_full_hostname "$s")
-   target_ip_address=$(get_ip_address "$s")
-   port_list=${s}_ports
-   for target_port in "${!port_list}"; do
-      printf "Target host/IP: $target_full_hostname ( $target_ip_address )\nTarget Port: $target_port \n"
-      nc -zv $target_ip_address $target_port
+ for target in "${targets_to_test[@]}" ; do
+
+   svr=$( echo $target | awk -F":" '{print $1'})
+   target_full_hostname=$(get_full_hostname "$svr")
+   target_ip_address=$(get_ip_address "$svr")
+   target_ports=$(echo $( echo $target | awk -F":" '{ for (i=2;i<=NF;i++) print $i }' ))
+   printf "To: $target_full_hostname ( $target_ip_address )\nTarget Port(s): $target_ports\n"
+   for port in ${target_ports[@]}; do
+       nc -zv $target_ip_address $port
    done
    echo -------------------------------------------------------------------------------------
-   ((i++))
  done
 }
 
